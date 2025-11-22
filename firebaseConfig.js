199a98f0423d3b9676ff3cf68192b2c778b012e0
Sreeja <86891472+ssenc-17@users.noreply.github.com>
2025-11-22 11:36:24 +0000

    Added firebase to project

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCFej6naQ_ExBapDPx0ZL_mHzfzUNp4sr0",
+    authDomain: "auth-3a64e.firebaseapp.com",
+    projectId: "auth-3a64e",
+    storageBucket: "auth-3a64e.firebasestorage.app",
+    messagingSenderId: "657257068436",
+    appId: "1:657257068436:web:d52ac8735c7216bf550181"
+};
